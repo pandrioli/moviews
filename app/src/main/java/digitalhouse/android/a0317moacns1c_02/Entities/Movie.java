843497db340f3b44ca0417ccbe9ed5bffd2df664
Unknown <pabloandrioli77@gmail.com>
2017-05-20 19:05:34 -0300

    Clases Movie, Cast, Crew con getters y setters
    Usé el mismo nombre de los campos que usa la API pero pasados a camelcase

--- app/src/main/java/digitalhouse/android/a0317moacns1c_02/Entities/Movie.java
+++ app/src/main/java/digitalhouse/android/a0317moacns1c_02/Entities/Movie.java
@@ -10,7 +10,7 @@ public class Movie {
     private Boolean adult;
     private String backdropPath;
     private Integer budget;
-    private List<String> genres;
+    private String genres;
     private String homepage;
     private Integer id;
     private String imdbId;
@@ -19,16 +19,229 @@ public class Movie {
     private String overview;
     private Double popularity;
     private String posterPath;
-    private List<String> productionCompanies;
-    private List<String> productionCountries;
+    private String productionCompanies;
+    private String productionCountries;
     private String releaseDate;
     private Integer revenue;
     private Integer runtime;
-    private List<String> spokenLanguages;
+    private String spokenLanguages;
     private String status;
     private String tagline;
     private String title;
     private Boolean video;
     private Double voteAverage;
     private Integer voteCount;
+    private List<Cast> castList;
+    private List<Crew> crewList;
+
+    public Movie() {
+    }
+
+    public Boolean getAdult() {
+        return adult;
+    }
+
+    public void setAdult(Boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public Integer getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Integer budget) {
+        this.budget = budget;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(Double popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getProductionCompanies() {
+        return productionCompanies;
+    }
+
+    public void setProductionCompanies(String productionCompanies) {
+        this.productionCompanies = productionCompanies;
+    }
+
+    public String getProductionCountries() {
+        return productionCountries;
+    }
+
+    public void setProductionCountries(String productionCountries) {
+        this.productionCountries = productionCountries;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Integer getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(Integer revenue) {
+        this.revenue = revenue;
+    }
+
+    public Integer getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(Integer runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getSpokenLanguages() {
+        return spokenLanguages;
+    }
+
+    public void setSpokenLanguages(String spokenLanguages) {
+        this.spokenLanguages = spokenLanguages;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTagline() {
+        return tagline;
+    }
+
+    public void setTagline(String tagline) {
+        this.tagline = tagline;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getVideo() {
+        return video;
+    }
+
+    public void setVideo(Boolean video) {
+        this.video = video;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public List<Cast> getCastList() {
+        return castList;
+    }
+
+    public void setCastList(List<Cast> castList) {
+        this.castList = castList;
+    }
+
+    public List<Crew> getCrewList() {
+        return crewList;
+    }
+
+    public void setCrewList(List<Crew> crewList) {
+        this.crewList = crewList;
+    }
 }
