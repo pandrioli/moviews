584c52d23eac1909aca033cbcfc4b1b1f1eef552
Unknown <pabloandrioli77@gmail.com>
2017-05-20 18:33:05 -0300

    Campos de la clase Movie
    Renombré la clase "Movies" a "Movie" - Agregué campos a la clase

---
+++ app/src/main/java/digitalhouse/android/a0317moacns1c_02/Entities/Movie.java
@@ -0,0 +1,34 @@
+package digitalhouse.android.a0317moacns1c_02.Entities;
+
+import java.util.List;
+
+/**
+ * Created by Gregorio Martin on 20/5/2017.
+ */
+
+public class Movie {
+    private Boolean adult;
+    private String backdropPath;
+    private Integer budget;
+    private List<String> genres;
+    private String homepage;
+    private Integer id;
+    private String imdbId;
+    private String originalLanguage;
+    private String originalTitle;
+    private String overview;
+    private Double popularity;
+    private String posterPath;
+    private List<String> productionCompanies;
+    private List<String> productionCountries;
+    private String releaseDate;
+    private Integer revenue;
+    private Integer runtime;
+    private List<String> spokenLanguages;
+    private String status;
+    private String tagline;
+    private String title;
+    private Boolean video;
+    private Double voteAverage;
+    private Integer voteCount;
+}
--- app/src/main/java/digitalhouse/android/a0317moacns1c_02/Entities/Movies.java
+++
@@ -1,8 +0,0 @@
-package digitalhouse.android.a0317moacns1c_02.Entities;
-
-/**
- * Created by Gregorio Martin on 20/5/2017.
- */
-
-public class Movies {
-}
