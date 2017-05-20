65e89727629fbc37fd5dcb5aa06624976b980264
Gregorio Martin <gregorio.martin92@gmail.com>
2017-05-20 18:15:31 -0300

    Primeros pkgs y clases agregadas
    Se agregaron los pckgs:
    Entities
    Helpers
    Security
    Services
    
    Ademas las clases:
    Movie
    MovieItemList
    MovieDetails
    MovieService
    MovieServiceImpl
    MovieMapper
    MovieMapperImpl
    
    Todo esto para empezar a trabajar en grupo.

---
+++ app/src/main/java/digitalhouse/android/a0317moacns1c_02/Entities/MovieDetails.java
@@ -0,0 +1,13 @@
+package digitalhouse.android.a0317moacns1c_02.Entities;
+
+import android.graphics.Movie;
+
+/**
+ * Created by Gregorio Martin on 20/5/2017.
+ */
+
+public class MovieDetails {
+
+    private Movie movie;
+
+}
---
+++ app/src/main/java/digitalhouse/android/a0317moacns1c_02/Services/MovieServiceImpl.java
@@ -0,0 +1,8 @@
+package digitalhouse.android.a0317moacns1c_02.Services;
+
+/**
+ * Created by Gregorio Martin on 20/5/2017.
+ */
+
+public class MovieServiceImpl implements MovieService {
+}
